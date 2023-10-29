09f3b2d2ff6cf1e18b284e3bf54a54397fb261fa
Terrasse <terrasse@qq.com>
2023-10-29 12:05:40 +0000

    remove useless TODO

--- kernel/bio.c
+++ kernel/bio.c
@@ -105,7 +105,7 @@ bget(uint dev, uint blockno)
         new_b = b;
         b->prev->next = b->next;
         b->next->prev = b->prev;
-        bcaches[i].head.next->prev = b->prev; // TODO
+        bcaches[i].head.next->prev = b->prev;
         bcaches[i].head.next = b->next;
         break;
       }
